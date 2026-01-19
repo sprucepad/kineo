fe39d415977d59883f8db166ef075e781f204090
sprucepad <sprucepadhq@gmail.com>
2026-01-19 20:05:51 -0300

    refactor!: organize imports

--- packages/kineo/src/index.ts
+++ packages/kineo/src/index.ts
@@ -1,6 +1,18 @@
-export * from "./client";
-export * from "./schema";
-export * from "./error";
-export * from "./model";
-export * from "./adapter";
-export * from "./plugin";
+export { Kineo, type InferClient } from "./client";
+export {
+  defineSchema,
+  model,
+  field,
+  relation,
+  type InferSchema,
+} from "./schema";
+export { KineoError, KineoErrorKind } from "./error";
+export { Model, GraphModel } from "./model";
+export type {
+  Adapter,
+  Compiler,
+  MigrationEntry,
+  MigrationCommand,
+  MigrationNote,
+} from "./adapter";
+export type { Plugin } from "./plugin";
--- packages/kineo/tsdown.config.ts
+++ packages/kineo/tsdown.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from "tsdown";
+import pkg from "./package.json";
 
-export default defineConfig([
-  {
-    entry: [
-      "src/index.ts",
-      "src/ir.ts",
-      "src/kit/index.ts",
-      "src/adapters/neo4j.ts",
-      "src/compilers/sql.ts",
-      "src/compilers/cypher.ts",
-    ],
-    external: ["neo4j-driver"],
-    sourcemap: true,
-    minify: true,
-  },
-  {
-    entry: ["src/kit/main.ts"],
-    dts: false,
-    minify: true,
-  },
-]);
+export default defineConfig({
+  entry: Object.values(pkg.exports).map((pkgExport) => pkgExport.development),
+  external: ["neo4j-driver"],
+  dts: true,
+  sourcemap: true,
+  minify: true,
+});
