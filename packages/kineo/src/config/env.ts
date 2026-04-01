f03cae6f47fe35364adb364e7dbd6f72e665d38c
sprucepad <sprucepadhq@gmail.com>
2026-04-01 14:30:16 -0300

    feat: remove dotenv in favor of node

--- packages/kineo/src/config/env.ts
+++ packages/kineo/src/config/env.ts
@@ -1,6 +1,4 @@
-import path from "node:path";
 import process from "node:process";
-import dotenv, { type DotenvConfigOptions } from "dotenv";
 
 export function env(key: string): string {
   const val = process.env[key];
@@ -14,24 +12,12 @@ export class UndefinedEnvError extends Error {
   }
 }
 
-export type EnvOptions = Omit<DotenvConfigOptions, "File">;
-
-export function loadEnv(opts: EnvOptions, ...files: string[]): void;
-export function loadEnv(...files: string[]): void;
-
-export function loadEnv(...args: [EnvOptions, ...string[]] | string[]): void {
-  let opts: EnvOptions | undefined;
-  let files: string[];
-
-  if (typeof args[0] === "string" || args.length === 0) {
-    opts = undefined;
-    files = args as string[];
-  } else {
-    opts = args[0];
-    files = args.slice(1) as string[];
-  }
-
+export function loadEnv(...files: string[]): void {
   for (const file of files) {
-    dotenv.config({ ...opts, path: path.join(process.cwd(), file) });
+    try {
+      process.loadEnvFile(file);
+    } catch {
+      continue;
+    }
   }
 }
