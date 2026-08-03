54b8b62018cfdec37f25d436bdf4eb44112d5911
sprucepad <sprucepadhq@gmail.com>
2026-08-03 17:54:41 -0300

    refactor: move to oxc-node to import config (that way imports are resolved correctly)

--- packages/core/kineo/src/main.ts
+++ packages/core/kineo/src/main.ts
@@ -1,18 +1,38 @@
-import { Command, i } from "convoker";
-import { greet } from ".";
-
 // To make Knip ignore this for now
 // -- Will be used in CLI
 import { text as _text } from "@clack/prompts";
-// -- Will be used in schema parsers
+// -- Will be used for schema parsing
+import _resolver from "oxc-resolver";
 import { parse as _parse } from "oxc-parser";
-import _resolve from "oxc-resolver";
+import { walk as _walk } from "oxc-walker";
+
+import process from "node:process";
+import path from "node:path";
+import { Command, i } from "convoker";
+
+import { greet } from ".";
+import { loadConfigs } from "./config/parser";
 
 new Command(
   "kineo",
   "An experimental, declarative TypeScript ORM.",
   "0.12.0-alpha",
 )
+  .input({
+    envFiles: i.option("string", "-e", "--env-files").list().optional(),
+    configs: i.option("string", "-c", "--configs").list().optional(),
+  })
+  .use(
+    async ({
+      envFiles = [".env", ".env.local", ".env.production"],
+      configs = [],
+    }) => {
+      for (const envFile of envFiles) {
+        process.loadEnvFile(path.resolve(process.cwd(), envFile));
+      }
+      loadConfigs(configs);
+    },
+  )
   .subCommand("greet", (c) =>
     c
       .description("Greets a set of users.")
